ca7c88b1ed581b14266f2a0afb01fcc7c8ea80e3
Luis Modesto <a20310068@ceti.mx>
2024-09-11 20:01:49 +0000

    remove wrong code

--- web_application/run.py
+++ web_application/run.py
@@ -7,5 +7,4 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from web_application import app
 
 if __name__ == '__main__':
-    pip install -r requirements.txt
     app.run(host='0.0.0.0', port=5000, debug=True)
