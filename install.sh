252fb2763f493422b40601c3a31b99192f30291e
Luis Modesto <a20310068@ceti.mx>
2024-10-06 23:55:11 +0000

    install setup

--- install.sh
+++ install.sh
@@ -8,6 +8,11 @@ sudo apt update
 echo "Upgrading packages..."
 sudo apt upgrade -y
 
+# Create and activate a virtual environment
+echo "Setting up virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
+
 # Install Python dependencies
 echo "Installing required Python packages..."
 cd web_application
