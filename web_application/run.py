9ba8b5d34a4378860e49db68cc5cbb31dc9454cd
Luis Modesto <a20310068@ceti.mx>
2024-10-19 17:24:52 +0000

    Add Install on run.py

--- web_application/run.py
+++ web_application/run.py
@@ -1,8 +1,20 @@
 import sys
 import os
+import subprocess
 
 # Add the parent directory to sys.path so Python can find 'utils'
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Define the path to the installation script
+install_script = os.path.join(os.path.dirname(__file__), '..', 'install.sh')
+
+# Run the install.sh script
+try:
+    print("Running installation script...")
+    result = subprocess.run(['bash', install_script], check=True)
+except subprocess.CalledProcessError as e:
+    print(f"Error occurred while running the installation script: {e}")
+    sys.exit(1)
 
 from web_application import app
 
